abf55464454fdce3ac28a602539e9da6e0f6611c
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-11-26 17:21:36 +0000

    Slide styling, responsive fonts, use mock data file

--- src/js/slides.js
+++ src/js/slides.js
@@ -1,58 +1,49 @@
-var _ = require('underscore');
+var _ = require('underscore'),
+  Dashboard = require('performanceplatform-client.js');
 
 module.exports = function (slideContainer) {
-  var dashboardConfig = {
-    'title': 'Carer\'s Allowance applications',
-    'slug': 'carers-allowance',
-    'department': {
-      'abbr': 'DWP',
-      'title': 'Department for Work and Pensions'
-    },
-    'modules': [
-      {
-        'title': 'Digital take-up',
-        'data': '56%'
-      },
-      {
-        'title': 'User satisfaction',
-        'data': '90%'
-      },
-      {
-        'title': 'Completion rate',
-        'data': '68%'
-      },
-      {
-        'title': 'Applications breakdown',
-        'data': '6,413'
-      }
-    ]
-  };
+  var dashboard = new Dashboard();
 
-  var html = '';
-
-  var introSlideTemplate = _.template('<h3>Performance data for <%= title %></h3><p>Data from www.gov.uk/performance</p>'),
-      slideTemplate = _.template('<h3><%= dashboardTitle %></h3><p><span class="big-number"><%= data %></span><%= title.toLowerCase() %><p class="dashboard-slug">https://www.gov.uk/performance/<%= dashboardSlug %></p>');
-
-  var introSlide = document.createElement('div');
-  introSlide.classList.add('slide');
-  introSlide.classList.add('intro-slide');
-  introSlide.classList.add('on-screen');
-  introSlide.innerHTML = introSlideTemplate(dashboardConfig);
-
-  html += introSlide.outerHTML;
-
-  for (var module in dashboardConfig.modules) {
-    var config = dashboardConfig.modules[module],
-        slide = document.createElement('div');
-
-    config.dashboardSlug = dashboardConfig.slug;
-    config.dashboardTitle = dashboardConfig.title;
-    slide.classList.add('slide');
-    slide.innerHTML = slideTemplate(config);
-    slide.classList.add(dashboardConfig.department.title.toLowerCase().replace(/ /g, '-'));
-    html += slide.outerHTML;
+  function getMockJson() {
+    var json = require('../../test/fixtures/dashboard-config.json');
+    for (var module in json.modules) {
+      json.modules[module].data = '20%';
+    }
+    return json;
   }
 
-  slideContainer.innerHTML = html;
+  return dashboard.getConfig('carers-allowance').
+    fin(function (dashboardConfig) {
+      dashboardConfig = getMockJson();
+      var html = '';
+      var introSlideTemplate = _.template('<h1>Performance data for <%= title %></h1><p class="data-from">Data from www.gov.uk/performance</p>'),
+        slideTemplate = _.template('<h2><%= dashboardTitle %></h2><p class="content"><span class="big-number"><%= data %></span> <span class="title"><%= title.toLowerCase() %></span><p class="dashboard-slug">www.gov.uk/performance/<%= dashboardSlug %></p>');
+
+      var introSlide = document.createElement('div');
+      introSlide.classList.add('slide');
+      introSlide.classList.add('intro-slide');
+      introSlide.classList.add('on-screen');
+      introSlide.innerHTML = introSlideTemplate(dashboardConfig);
+
+      html += introSlide.outerHTML;
+
+      for (var module in dashboardConfig.modules) {
+        var config = dashboardConfig.modules[module],
+          slide = document.createElement('div');
+
+        config.dashboardSlug = dashboardConfig.slug;
+        config.dashboardTitle = dashboardConfig.title;
+        slide.classList.add('slide');
+
+        slide.innerHTML = slideTemplate(config);
+        slide.classList.add(dashboardConfig.department.abbr.toLowerCase());
+        html += slide.outerHTML;
+      }
+
+      slideContainer.innerHTML = html;
+
+    }, function (err) {
+      throw(err);
+    });
 
 };
