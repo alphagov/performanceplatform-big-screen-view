0d54232f21a916b040b87b3c96c0687568d0e9cc
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2014-11-27 09:59:03 +0000

    Increase slide display interval to 10s

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 var fullscreen = require('./fullscreen')(document.getElementById('fs'));
 var slides = require('./slides')(document.getElementById('fs')).then(function() {
   var carousel = require('./carousel');
-  var slideRotationInterval = window.setInterval(carousel, (2 * 1000));
+  var slideRotationInterval = window.setInterval(carousel, (5 * 1000));
 });
 var analytics = require('./analytics');
 
