9a227adc28a5181b430cbcffef4b9b3e36a65707
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-03-11 16:03:33 +0000

    Use new frontend toolkit analytics

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,10 @@ require('./slides')(dashboardSlug, container).then(function () {
 var fullscreen = require('./fullscreen')(container);
 document.getElementById('full-screen-toggle').onclick = fullscreen;
 
+require('govuk_frontend_toolkit/javascripts/govuk/analytics/google-analytics-classic-tracker');
+require('govuk_frontend_toolkit/javascripts/govuk/analytics/google-analytics-universal-tracker');
+require('govuk_frontend_toolkit/javascripts/govuk/analytics/tracker');
+require('./analytics-init');
 var analytics = require('./analytics');
 analytics.setup();
 
--- test/js/analytics.spec.js
+++ test/js/analytics.spec.js
@@ -3,17 +3,22 @@ describe('analytics', function () {
   beforeEach(function () {
     var analytics;
     this.clock = sinon.useFakeTimers();
-    window.ga = function () { };
-    this.spy = sinon.spy(window, 'ga');
+    window.GOVUK = {
+      analytics: {
+        trackEvent: function() {}
+      }
+    };
+    this.spy = sinon.spy(window.GOVUK.analytics, 'trackEvent');
     analytics = require('../../src/js/analytics');
     analytics.setup();
     this.clock.tick(analytics.interval + 500);
   });
 
   it('should send the elapsed time and action with the GA event', function () {
-    var event = this.spy.args[0][1];
+    console.log(this.spy.args[0]);
+    var callArgs = this.spy.args[0];
     this.spy.calledOnce.should.equal(true);
-    event.eventValue.should.equal(10);
-    event.eventAction = 'minutes-since-page-load';
+    callArgs[2].value.should.equal(10);
+    callArgs[1].should.equal('minutes-since-page-load');
   });
 });
