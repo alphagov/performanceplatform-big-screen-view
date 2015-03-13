d3351ee95c6c1b17b9cc5d11435bdbc40eaa09f8
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-03-13 12:16:03 +0000

    Use universal tracker only

--- src/js/analytics-init.js
+++ src/js/analytics-init.js
@@ -2,18 +2,14 @@
   'use strict';
 
   // Load Google Analytics libraries
-  GOVUK.Tracker.load();
+  GOVUK.GoogleAnalyticsUniversalTracker.load();
 
   // Use document.domain in dev, preview and staging so that tracking works
   // Otherwise explicitly set the domain as www.gov.uk (and not gov.uk).
   var cookieDomain = (document.domain === 'www.gov.uk') ? '.www.gov.uk' : document.domain;
 
   // Configure profiles, setup custom vars, track initial pageview
-  GOVUK.analytics = new GOVUK.Tracker({
-    universalId: 'UA-XXXXXXX-X',
-    classicId: 'UA-26179049-9',
-    cookieDomain: cookieDomain
-  });
+  GOVUK.analytics = new GOVUK.GoogleAnalyticsUniversalTracker('UA-26179049-9', cookieDomain);
 
   // Set custom dimensions before tracking pageviews
   if (window.devicePixelRatio) {
--- src/js/app.js
+++ src/js/app.js
@@ -9,9 +9,7 @@ require('./slides').setup(dashboardSlug, container).then(function () {
 var fullscreen = require('./fullscreen')(container);
 document.getElementById('full-screen-toggle').onclick = fullscreen;
 
-require('govuk_frontend_toolkit/javascripts/govuk/analytics/google-analytics-classic-tracker');
 require('govuk_frontend_toolkit/javascripts/govuk/analytics/google-analytics-universal-tracker');
-require('govuk_frontend_toolkit/javascripts/govuk/analytics/tracker');
 require('./analytics-init');
 var analytics = require('./analytics');
 analytics.setup();
