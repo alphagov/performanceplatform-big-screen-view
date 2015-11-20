e9e7aa2268c452e4683a659a7badc011e17b7c83
Lee Longmore <lee.longmore@gmail.com>
2015-11-20 11:24:16 +0000

    Fix broken Big Screen View on IE9
    - classList.add|remove not supported

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -5,13 +5,13 @@ module.exports = function (container) {
   setTimer = function () {
     clearTimeout(timer);
     timer = setTimeout(function () {
-      container.classList.add('is-inactive');
+      container.className += " is-inactive";
     }, 5000);
   };
 
   setTimer();
   container.addEventListener && container.addEventListener('mousemove', function () {
-    container.classList.remove('is-inactive');
+    container.className = container.className.replace(" is-inactive", "");
     setTimer();
   });
 
